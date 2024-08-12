6bce3efc3b2ab6a3f28f56b21cf1aabc9c8137e8
henrivilar <fabio.fv1@hotmail.com>
2024-08-12 08:55:59 -0300

    add Agendamento

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,21 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button" 
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return <div>
-    {/* header */}
+    {/* Header */}
     <Header />
+
+    {/* Texto */}
     <div className="p-5">
       <h2 className="text-xl font-bold">Olá, Henrique!</h2>
       <p>Sábado, 10 de Agosto</p>
 
+      {/* Busca */}
       <div className="flex items-center gap-2 mt-6">
         <Input placeholder="Faça sua busca..." />
         <Button>
@@ -19,9 +25,35 @@ const Home = () => {
         </Button>
       </div>
 
+      {/* Banner */}
       <div className="relative w-full h-[150px] mt-6">
         <Image alt="Agende nos melhores com FSW Barber" src="/Banner-01.png" fill className="object-cover rounded-xl" />
       </div>
+
+      {/* Agendamento */}
+      <Card className="mt-6">
+        <CardContent className="flex justify-between p-0">
+          {/* Esquerda */}
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+              </Avatar>
+              <p className="text-sm">Barbearia FSW</p>
+            </div>
+          </div>
+
+          {/* Direita */}
+          <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">12</p>
+            <p className="text-sm">08:47</p>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   </div>
 }
